f635dd6a5f892f91216ca1d0e18cd8375a326f32
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-08-11 16:49:12 -0600

    first commit

---
+++ Asistencia/Asistencia/src/main/java/esfe/com/Asistencia/Repositorios/IGrupoRepository.java
@@ -0,0 +1,7 @@
+package esfe.com.Asistencia.Repositorios;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import esfe.com.Asistencia.Modelos.Grupo;
+public interface IGrupoRepository extends JpaRepository<Grupo, Integer> {
+    
+}
