d8219912267d3b97aff7cf0c1523011af04adf34
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-08-18 14:20:58 -0600

    Agregar controlador, servicio y repositorio para gestionar asignaciones de docentes, junto con las vistas correspondientes.

---
+++ Asistencia/Asistencia/src/main/java/esfe/com/Asistencia/Servicios/Interfaces/IDocenteGrupoService.java
@@ -0,0 +1,21 @@
+package esfe.com.Asistencia.Servicios.Interfaces;
+
+import java.util.*;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import esfe.com.Asistencia.Modelos.DocenteGrupo;
+
+public interface IDocenteGrupoService {
+
+    List<DocenteGrupo> obtenerTodos();
+
+    Page<DocenteGrupo> buscarTodosPaginados(Pageable pageable);
+
+    DocenteGrupo buscarPorId(Integer id);
+
+    DocenteGrupo crearOEditar(DocenteGrupo docenteGrupo);
+
+    void eliminarPorId(Integer id);
+}
