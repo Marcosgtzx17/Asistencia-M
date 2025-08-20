9303a6c01b48537a3811121d0c1521bf7f073d8a
Marcos Gutiérrez <marcosgtzx7@hotmail.com>
2025-08-20 16:29:19 -0600

    Agregar funcionalidad de gestión de usuarios y roles, incluyendo controladores, servicios y vistas para crear, editar y listar usuarios. Implementar seguridad con Spring Security y formularios de inicio de sesión.

--- Asistencia/Asistencia/src/main/java/esfe/com/Asistencia/Controladores/homeController.java
+++ Asistencia/Asistencia/src/main/java/esfe/com/Asistencia/Controladores/homeController.java
@@ -1,8 +1,11 @@
 package esfe.com.Asistencia.Controladores;
 
+import org.springframework.security.web.authentication.logout.SecurityContextLogoutHandler;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpServletRequest;
 @Controller
 @RequestMapping("/")
 public class homeController {
@@ -11,4 +14,16 @@ public class homeController {
     public String index() {
         return "Home/Index";
     }   
+
+     @GetMapping("/login" )
+    public String mostrarLogin() {
+        return "home/formLogin";
+    }
+
+    @GetMapping("/logout")
+    public String logout(HttpServletRequest request){
+        SecurityContextLogoutHandler logoutHandler = new SecurityContextLogoutHandler();
+        logoutHandler.logout(request, null, null);
+        return "redirect:/";
+    }
 }
---
+++ Asistencia/Asistencia/src/main/java/esfe/com/Asistencia/Repositorios/IUsuarioRepository.java
@@ -0,0 +1,7 @@
+package esfe.com.Asistencia.Repositorios;
+import org.springframework.data.jpa.repository.JpaRepository;
+import esfe.com.Asistencia.Modelos.Usuario;
+
+public interface IUsuarioRepository extends JpaRepository<Usuario, Integer> {
+    
+}
